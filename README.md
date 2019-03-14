0a5843c967314264a71f525030fd49ffb814bc37
Floyd Orr <floydcanhelp@gmail.com>
2019-03-14 07:11:51 -0500

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# React-UI
-Created with CodeSandbox
---
+++ src/App.js
@@ -0,0 +1,123 @@
+import React, { Component } from "react";
+import "./App.css";
+import { Container, Row, Col, ListGroup, ListGroupItem } from "reactstrap";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      data: [],
+      error: [],
+      artist: []
+    };
+  }
+  componentDidMount() {
+    fetch("https://fb-assessment.glitch.me/artists")
+      .then(response => response.json())
+      .then(data => this.setState({ data: data }))
+      .catch(error => this.setState({ error: "info not found" }));
+  }
+  handleClick = e => {
+    let artist = this.state.data[e.target.id];
+    this.setState({ artist: artist });
+  };
+  render() {
+    const { data, artist } = this.state;
+    let artperson = [];
+    let nullOb = "";
+    Object.keys(artist).length > 0
+      ? (artperson = artist)
+      : (nullOb = artperson);
+
+    const divStyle = {
+      position: "absolute",
+      right: "20%",
+      height: "30%",
+      top: "20%",
+      marginLeft: "5%",
+      marginTop: "-100px",
+      padding: "5px 0px 0px 5px",
+      marginRight: "0",
+      clear: "both"
+    };
+
+    return (
+      <Container>
+        {data.map((data, artistID) => (
+          <div key={data.artistID}>
+            <div style={{ width: "350px", height: "100px" }}>
+              <ListGroup>
+                <ListGroupItem tag="a" href="#">
+                  <Col sm="6" xs="12" className="color">
+                    <img
+                      id={artistID}
+                      onClick={this.handleClick}
+                      className="rounded-circle textCenter"
+                      src={data.imageURL}
+                      width="50px"
+                      height="50"
+                      alt="img"
+                    />
+                  </Col>
+                  <Col
+                    sm="6"
+                    xs="12"
+                    style={{
+                      position: "reletive",
+                      top: "-50px",
+                      width: "300px",
+                      left: "60px",
+                      marginBottom: "0"
+                    }}
+                  >
+                    <p id={artistID} onClick={this.handleClick}>
+                      {" "}
+                      {data.firstName} {data.lastName}{" "}
+                    </p>
+                    <p id={artistID} onClick={this.handleClick}>
+                      {" "}
+                      {data.art}{" "}
+                    </p>
+                  </Col>
+                </ListGroupItem>
+              </ListGroup>
+            </div>
+          </div>
+        ))}
+
+        <Row style={divStyle}>
+          <Col>
+            {artperson === artist && (
+              <React.Fragment>
+                <div
+                  style={{
+                    border: "thin solid black",
+                    paddingBottom: "10px",
+                    PaddingTop: "5px",
+                    marginLeft: "700px"
+                  }}
+                  className="text-center"
+                >
+                  <h3>
+                    {artperson.firstName} {artperson.lastName}
+                  </h3>
+                  <p className="img-fluid">{artperson.art}</p>
+                  <img
+                    className="img-fluid"
+                    src={artperson.imageURL}
+                    width="100%"
+                    height="100%"
+                    alt="img"
+                  />
+                </div>
+              </React.Fragment>
+            )}
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
+}
+
+export default App;
